b7215ba19ae0974d053c1c5b10a3923b8f7b7146
IhsanMac <88120180+ihsanpraditya@users.noreply.github.com>
2023-05-20 15:19:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# litexl-dotfiles
+
+I don't think you need this. You must be able to understand the ease of [Lite XL](https://github.com/lite-xl/lite-xl) configuration.
---
+++ user_settings.lua
@@ -0,0 +1,31 @@
+return {
+  ["config"] = {
+    ["code_font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "JetBrains Mono Regular",
+          ["path"] = "/Applications/Lite XL.app/Contents/Resources/fonts/JetBrainsMono-Regular.ttf"
+        },
+        [2] = {
+          ["name"] = "CaskaydiaCove Nerd Font Regular",
+          ["path"] = "/Users/ihsanpraditya/Library/Fonts/Caskaydia Cove Nerd Font Complete Regular.otf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["plugins"] = {
+      ["linewrapping"] = {
+        ["enable_by_default"] = true
+      }
+    }
+  }
+}
